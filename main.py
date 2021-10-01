e9759ef8fba16bfecc28d64d540886a62985c6f7
brian <78578795+ap-actual@users.noreply.github.com>
2021-10-01 15:24:07 -0400

    update todo's for Schmieder
    update todo's from zoom

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from datetime import datetime
 
 def getfile():
 
-    filename = askopenfilename()
-    return filename
+    filepath = askopenfilename()
+    return filepath
 
 
 def readfile(fname):
+
+    # TODO: input checks & error handling
 
     try:
         ans = pd.read_csv(fname)
@@ -25,24 +27,25 @@ def readfile(fname):
 if __name__ == '__main__':
 
     fname = getfile()
-    ans = readfile(fname)
+    df = readfile(fname)
 
     # Go through each rehearsal
-    for col in ans.columns:
+    for col in df.columns:
 
         # if header contains the word 'Rehearsal', then it's a rehearsal!
         if 'Rehearsal' in str(col):
+            # pull out rehearsal start & end time from column heading string
             col_str = str(col)
             col_str = col_str.split('\n')
             rehearsal_time = col_str[2]
             rehearsal_time = rehearsal_time.split('-')
             rehearsal_start = rehearsal_time[0]
             rehearsal_end = rehearsal_time[1]
-            rehearsal_start_obj = datetime.strptime(str(rehearsal_start), '%I:%M %p')
-            missed_minutes_col =  str('Minutes missed at ' + col_str[1])
-            ans.insert(len(ans.columns), missed_minutes_col, 0)
+            rehearsal_start_obj = datetime.strptime(str(rehearsal_start), '%I:%M %p') # convert string to datetime
+            missed_minutes_col =  str('Minutes missed at ' + col_str[1]) # add column to existing df
+            df.insert(len(df.columns), missed_minutes_col, 0) # insert column at end of df
 
-            for index, row in ans.iterrows():
+            for index, row in df.iterrows():
 
                 # handle tardy entries
                 if 'Tardy' in str(row[col]):
@@ -51,6 +54,7 @@ if __name__ == '__main__':
                     time_in_obj = datetime.strptime(time_in, ' %I:%M %p')
                     time_missed = time_in_obj - rehearsal_start_obj
                     time_missed_minutes = time_missed.total_seconds()/60
-                    ans.at[index,missed_minutes_col] = time_missed_minutes
+                    df.at[index, missed_minutes_col] = time_missed_minutes
 
-ans.to_csv(str('OUTPUT.csv'))
+# TODO: make this better
+df.to_csv(str('OUTPUT.csv'))
