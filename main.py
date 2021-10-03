a37f28592b7ae93592102a60a856305e85fbb7bb
brian <78578795+ap-actual@users.noreply.github.com>
2021-10-03 12:18:40 -0400

    Handle DNCI and EBD's, add comment column
    handle DNCI and EBD entries and also has comment column with comments

--- main.py
+++ main.py
@@ -42,8 +42,11 @@ if __name__ == '__main__':
             rehearsal_start = rehearsal_time[0]
             rehearsal_end = rehearsal_time[1]
             rehearsal_start_obj = datetime.strptime(str(rehearsal_start), '%I:%M %p') # convert string to datetime
+            rehearsal_end_obj   = datetime.strptime(str(rehearsal_end), '%I:%M %p') # convert string to datetime
             missed_minutes_col =  str('Minutes missed at ' + col_str[1]) # add column to existing df
+            comments_col = str('Comments on ' + col_str[1]) # add column to existing df
             df.insert(len(df.columns), missed_minutes_col, 0) # insert column at end of df
+            df.insert(len(df.columns), comments_col, '')  # insert column at end of df
 
             for index, row in df.iterrows():
 
@@ -54,7 +57,24 @@ if __name__ == '__main__':
                     time_in_obj = datetime.strptime(time_in, ' %I:%M %p')
                     time_missed = time_in_obj - rehearsal_start_obj
                     time_missed_minutes = time_missed.total_seconds()/60
-                    df.at[index, missed_minutes_col] = time_missed_minutes
+                    df.at[index, comments_col] = 'Missed ' + str(time_missed_minutes) + ' mins for signing in on UDB at ' + time_in
+
+                # handle DNCI entries
+                elif 'DNCI' in str(row[col]):
+                    time_missed = rehearsal_end_obj-rehearsal_start_obj
+                    time_missed_minutes = time_missed.total_seconds()/60
+                    df.at[index, comments_col] = 'Missed ' + str(time_missed_minutes) + ' (entire rehearsal) because of DNCI'
+
+                elif 'EBD' in str(row[col]):
+                    time_missed_minutes = -999
+                    df.at[index, comments_col] = 'EBD'
+
+                else:
+                    time_missed_minutes = 0
+
+                df.at[index, missed_minutes_col] = time_missed_minutes
+
+
 
 # TODO: make this better
 df.to_csv(str('OUTPUT.csv'))
