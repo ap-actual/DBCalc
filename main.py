2b7c895f0cddf3f017a2ea3528f5634d10571fb3
brian <78578795+ap-actual@users.noreply.github.com>
2021-10-04 21:53:18 -0400

    insert allowance .csv check

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from tkinter.filedialog import askopenfilename
+from tkinter import messagebox
 import pandas as pd
 from datetime import datetime
 
 
-def getfile():
+def getUDBfile():
 
+    answer = messagebox.askokcancel("UDB Output Select", "Hello! Please select UDB output file...")
+    filepath = askopenfilename()
+    return filepath
+
+def getAllowancefile():
+
+    answer = messagebox.askokcancel("UDB Output Select", "Now select Allowance file...")
     filepath = askopenfilename()
     return filepath
 
@@ -26,8 +34,10 @@ def readfile(fname):
 
 if __name__ == '__main__':
 
-    fname = getfile()
+    fname = getUDBfile()
+    fname_allow = getAllowancefile()
     df = readfile(fname)
+    df_allow = readfile(fname_allow)
 
     # Go through each rehearsal
     for col in df.columns:
@@ -41,11 +51,13 @@ if __name__ == '__main__':
             rehearsal_time = rehearsal_time.split('-')
             rehearsal_start = rehearsal_time[0]
             rehearsal_end = rehearsal_time[1]
-            rehearsal_start_obj = datetime.strptime(str(rehearsal_start), '%I:%M %p') # convert string to datetime
-            rehearsal_end_obj   = datetime.strptime(str(rehearsal_end), '%I:%M %p') # convert string to datetime
-            missed_minutes_col =  str('Minutes missed at ' + col_str[1]) # add column to existing df
-            comments_col = str('Comments on ' + col_str[1]) # add column to existing df
-            df.insert(len(df.columns), missed_minutes_col, 0) # insert column at end of df
+            rehearsal_start_obj = datetime.strptime(str(rehearsal_start), '%I:%M %p')  # convert string to datetime
+            rehearsal_end_obj   = datetime.strptime(str(rehearsal_end), '%I:%M %p')  # convert string to datetime
+            day_of_week = col_str[1].split(',')  # get day of week
+            day_of_week = day_of_week[0]  # get day of week
+            missed_minutes_col =  str('Minutes missed at ' + col_str[1])  # add column to existing df
+            comments_col = str('Comments on ' + col_str[1])  # add column to existing df
+            df.insert(len(df.columns), missed_minutes_col, 0)  # insert column at end of df
             df.insert(len(df.columns), comments_col, '')  # insert column at end of df
 
             for index, row in df.iterrows():
@@ -55,9 +67,21 @@ if __name__ == '__main__':
                     time_in = str(row[col]).split('|')
                     time_in = time_in[1]
                     time_in_obj = datetime.strptime(time_in, ' %I:%M %p')
-                    time_missed = time_in_obj - rehearsal_start_obj
-                    time_missed_minutes = time_missed.total_seconds()/60
-                    df.at[index, comments_col] = 'Missed ' + str(time_missed_minutes) + ' mins for signing in on UDB at ' + time_in
+
+                    # check if time in is after allowed time
+                    tardy_student = df_allow.loc[(df_allow['Last name'] == row['Last name']) & (df_allow['First name'] == row['First name'])]
+                    time_in_allowed = tardy_student[day_of_week+str(' Arrive')]
+                    time_in_allowed_str = str(time_in_allowed.iloc[0])
+                    time_in_allowed = datetime.strptime(str(time_in_allowed.iloc[0]), '%H:%M')
+
+                    if time_in_obj - rehearsal_start_obj > time_in_allowed - rehearsal_start_obj:
+                        time_missed = time_in_obj - rehearsal_start_obj
+                        time_missed_minutes = time_missed.total_seconds()/60
+                        df.at[index, comments_col] = 'Missed ' + str(time_missed_minutes) + ' mins for signing in on UDB at ' + time_in + ' . They were allowed to arrive at ' + str(time_in_allowed_str)
+
+                    else:
+                        time_missed = 0
+                        df.at[index, comments_col] = str(time_missed)
 
                 # handle DNCI entries
                 elif 'DNCI' in str(row[col]):
@@ -73,8 +97,6 @@ if __name__ == '__main__':
                     time_missed_minutes = 0
 
                 df.at[index, missed_minutes_col] = time_missed_minutes
-
-
 
 # TODO: make this better
 df.to_csv(str('OUTPUT.csv'))
